1b4a43a127c1ab8d6fd09784e3ca46603529bb7a
jenifer5 <sonnia-55@hotmail.com>
2025-07-08 12:55:10 +0200

    tema 4 mongodb correcciones

--- Tema4-MongoDBqueries/restaurants/consultas.js
+++ Tema4-MongoDBqueries/restaurants/consultas.js
@@ -6,7 +6,7 @@ db.restaurantes.find({borough:"Bronx"}).pretty();
 db.restaurantes.find({borough:"Bronx"}).limit(5).pretty();
 db.restaurantes.find({borough:"Bronx"}).skip(5).limit(5).pretty();
 db.restaurantes.find({"grades.score":{$gt:90}}).pretty();
-db.restaurantes.find({"grades.score":{$gt:80,$lt:100}}).pretty();
+db.restaurantes.find({grades:{$elemMatch:{score:{$gt:80,$lt:100}}}}).pretty();
 db.restaurantes.find({"address.coord.0":{$lt:-95.754168}}).pretty();
 db.restaurantes.find({cuisine:{$ne:"American"},"grades.score":{$gt:70},"address.coord.1":{$lt:-65.754168}}).pretty();
 db.restaurantes.find({cuisine:{$ne:"American"},"grades.score":{$gt:70},"address.coord.0":{$lt:-65.754168}}).pretty();
@@ -15,3 +15,5 @@ db.restaurantes.find({name:{$regex:/^Wil/}},{restaurant_id:1,name:1,borough:1,cu
 db.restaurantes.find({name:{$regex:/ces$/}},{restaurant_id:1,name:1,borough:1,cuisine:1,_id:0}).pretty();
 db.restaurantes.find({name:{$regex:/Reg/}},{restaurant_id:1,name:1,borough:1,cuisine:1,_id:0}).pretty();
 db.restaurantes.find({borough:"Bronx",cuisine:{$in:["American","Chinese"]}}).pretty();
+db.restaurantes.find({borough:{$in:["Staten Island","Queens","Bronx","Brooklyn"]}},{_id:0,restaurante_id:1,name:1,borough:1,cuisine:1}).pretty();
+db.restaurantes.find({borough:{$nin:["Staten Island","Queens","Bronx","Brooklyn"]}},{_id:0,restaurante_id:1,name:1,borough:1,cuisine:1}).pretty();
